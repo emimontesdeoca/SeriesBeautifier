45d41fa2f712d635bda85c2383845f7654c1d517
Emiliano Montesdeca <emontesdeoc@gmail.com>
2018-04-05 16:01:50 +0100

    Sorting and capitalizing implemented

--- AnimeBeautify/MainWindow.xaml.cs
+++ AnimeBeautify/MainWindow.xaml.cs
@@ -32,21 +32,20 @@ namespace AnimeBeautify
         public MainWindow()
         {
             InitializeComponent();
+
+            /// HELLO
             log.Text += Log.getLog("Welcome to SeriesBeautifier (づ￣ ³￣)づ");
             log.Text += Log.getLog("Everything is loaded");
             log.ScrollToHome();
+
+            /// Style
             beautifybtn.IsEnabled = false;
             progressbar.Value = 0;
             progressbar.Maximum = MaximumProgressBar;
-
-
         }
-
-
 
         private void searchbtn_Click(object sender, RoutedEventArgs e)
         {
-
             log.Text = "";
 
             /// Set maximum progress bar to 0
@@ -55,8 +54,6 @@ namespace AnimeBeautify
             /// Log
             log.Text += Log.getLog("Looking in " + foldertxt.Text);
             folderpath = foldertxt.Text;
-
-
 
             try
             {
@@ -77,8 +74,13 @@ namespace AnimeBeautify
                     log.Text += Log.getLog(i);
                     totalfound++;
 
+                    /// Get file count for progress bar
                     int filecounter = Directory.GetFiles(item).Count();
+
+                    /// L O G S
                     log.Text += Log.getLog("Found " + filecounter + " files inside");
+
+                    /// Progressbar
                     MaximumProgressBar += filecounter;
                     totalfound += filecounter;
 
@@ -93,6 +95,7 @@ namespace AnimeBeautify
                 log.Text += Log.getLog("Total found " + totalfound + " items");
                 log.Text += Log.getLog("Press Beautify to start");
 
+                /// Style stuff
                 progressbar.Maximum = MaximumProgressBar;
                 log.ScrollToEnd();
                 beautifybtn.IsEnabled = true;
@@ -106,71 +109,174 @@ namespace AnimeBeautify
 
         private async void beautifybtn_Click(object sender, RoutedEventArgs e)
         {
+            ///Log 
             log.Text += await Log.getLogAsync("Starting process for folders");
 
+            /// Dictionary for before and after
             Dictionary<string, string> changes = new Dictionary<string, string>();
 
             foreach (var item in folders)
             {
+                /// Get new folder name
                 string resFolder = await Beautifier.beautifyString(item);
+
+                /// Log
                 log.Text += await Log.getLogAsync("New folder name: " + resFolder);
+
+                /// Progress bar to make it async
                 progressbar.Value++;
 
+                /// Add to dictionary
                 changes.Add(item, resFolder);
             }
 
+            /// Some ints for S T A T I S T I C S
             int totalitemschanged = 0;
             int totalexceptions = 0;
 
+            /// Foreach in dictionary
             foreach (var item in changes)
             {
+                /// Get before and after, as they are necessary parameters for directory move method
                 string before = folderpath + "\\" + item.Key;
-                string after = folderpath + "\\" + item.Value;
+                string name = item.Value;
 
+                /// If capitalize
+                if (capitalize.IsChecked == true)
+                {
+                    /// L O G
+                    log.Text += await Log.getLogAsync("CapItAliZinG ¯\\(°_o)/¯");
+
+                    /// Capitalize
+                    name = name.First().ToString().ToUpper() + name.Substring(1);
+                }
+
+                /// Get whatever style 
+                string after = folderpath + "\\" + name;
+
+                /// If it does exist
                 if (!Directory.Exists(after))
                 {
                     try
                     {
+                        /// Rename
                         Directory.Move(before, after);
 
+                        /// Log
                         log.Text += await Log.getLogAsync("Starting process for files inside this folder");
+
+                        /// STATISTICS
                         totalitemschanged++;
-                        /// beautfy inside elements
+
+                        /// Beautify inside elements
                         foreach (var file in Directory.GetFiles(folderpath + "\\" + item.Value).ToList())
                         {
-
                             try
                             {
+                                /// Before and after, in order to use move method
                                 string fileold = file.Split('\\').Last();
                                 string filenew = await Beautifier.beautifyString(fileold);
 
+                                /// Get parent folder
                                 string parentfolder = Directory.GetParent(file).FullName;
+
+                                /// L O G S
                                 log.Text += await Log.getLogAsync("New file name: " + filenew);
 
+                                /// Rename
                                 File.Move(parentfolder + "\\" + fileold, parentfolder + "\\" + filenew);
+
+                                /// STATISTICS
                                 totalitemschanged++;
                             }
                             catch (Exception)
                             {
+                                /// Log
                                 log.Text += await Log.getLogAsync("There was an exception 乁( ◔ ౪◔)「      ┑(￣Д ￣)┍");
-                                totalexceptions++;
 
+                                /// STATISTICS
+                                totalexceptions++;
                             }
                         }
                     }
                     catch (Exception)
                     {
+                        /// Log
                         log.Text += await Log.getLogAsync("There was an exception 乁( ◔ ౪◔)「      ┑(￣Д ￣)┍");
-                        totalexceptions++;
 
+                        /// STATISTICS
+                        totalexceptions++;
                     }
+                }
+                else
+                {
+                    /// Log
+                    log.Text += await Log.getLogAsync("The folder already exist!");
+
+                    /// STATISTICS
+                    totalexceptions++;
                 }
             }
 
+            /// Logs
             log.Text += await Log.getLogAsync("Process finished");
             log.Text += await Log.getLogAsync("Renamed " + totalitemschanged + " items of " + totalfound + " total with " + totalexceptions + " exceptions");
+
+            /// Sorting
+            if (sort.IsChecked == true)
+            {
+                ///  L O G
+                log.Text += await Log.getLogAsync("Starting folder sort");
+
+                /// Get folders again
+                string[] folders = Directory.GetDirectories(foldertxt.Text);
+
+                /// root path of everything
+                string defaultpath = foldertxt.Text + "\\";
+
+                foreach (var item in folders)
+                {
+                    /// Get folder name
+                    string folder = item.Replace(foldertxt.Text, "").Replace("\\", "");
+
+                    /// Get folder uppercase letter
+                    string letter = folder[0].ToString().ToUpper();
+
+                    /// Fixes for folders
+                    if (letter == "." || letter == "?" || letter == "." || letter == ">" || letter == "<" || letter == "*" || letter == "\"" || letter == "/" || letter == ":" || letter == "|")
+                    {
+                        letter = "$%@";
+                    }
+
+                    /// Name
+                    string directoryname = folder.Split('\\').Last();
+
+                    /// Final location
+                    string finalplace = defaultpath + letter + '\\' + directoryname;
+
+                    /// Directory does exist
+                    if (Directory.Exists(defaultpath + letter))
+                    {
+                        /// M O V E
+                        Directory.Move(defaultpath + folder, finalplace);
+                    }
+                    else
+                    {
+                        /// Create directory first
+                        Directory.CreateDirectory(defaultpath + letter);
+
+                        // M O V E
+                        Directory.Move(defaultpath + folder, finalplace);
+                    }
+                }
+                /// L O G S B A B Y
+                log.Text += await Log.getLogAsync("Finished sort");
+            }
+
+            /// B Y E
             log.Text += await Log.getLogAsync("Thanks for using me, have a good day ༼ つ ◕_◕ ༽つ");
 
+            /// Styling
             progressbar.Value = MaximumProgressBar;
             log.ScrollToEnd();
             beautifybtn.IsEnabled = false;
